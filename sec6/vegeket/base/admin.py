e4dcadeed5fd4e6959cec4b7ca491595e5bb5df1
taku999hub <fuji3.man.zzzzz@gmail.com>
2021-07-16 11:28:06 +0900

    Update admin.py

--- sec6/vegeket/base/admin.py
+++ sec6/vegeket/base/admin.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import Group
 from base.models import Item, Category, Tag
 
 
-admin.site.register(Item)
+class TagInline(admin.TabularInline):
+    model = Item.tags.through
+
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+    exclude = ('tags',)
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.unregister(Group)  # 元からあるグループを使わないので非表示に
