9595ce0f7dff8e4f533bd2051d7a41402128aaaf
takux <53621441+takux@users.noreply.github.com>
2022-04-08 20:00:17 +0900

    add queryset

--- sec13/vegeket/base/views/item_views.py
+++ sec13/vegeket/base/views/item_views.py
@@ -6,6 +6,7 @@ from base.models import Item, Category, Tag
 class IndexListView(ListView):
     model = Item
     template_name = 'pages/index.html'
+    queryset = Item.objects.filter(is_published=True)
 
 
 class ItemDetailView(DetailView):
--- sec15/vegeket/base/views/item_views.py
+++ sec15/vegeket/base/views/item_views.py
@@ -5,6 +5,7 @@ from base.models import Item, Category, Tag
 class IndexListView(ListView):
     model = Item
     template_name = 'pages/index.html'
+    queryset = Item.objects.filter(is_published=True)
 
 
 class ItemDetailView(DetailView):
